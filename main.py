507f45a46fa30ed311cc51a84afd1d3d6d03a3b4
kurokuman <kurosawa.tomoyuki0812@gmail.com>
2020-10-15 00:36:17 +0900

    flask ベース api

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import jsonify
+from DataStore.MySQL import MySQL
+dns = {
+    'user': 'root',
+    'host': 'localhost',
+    'password': '',
+    'database': 'db1'
+}
+db = MySQL(**dns)
+
+app = Flask(__name__)
+# jsonで返却する値の文字コードをUTF-8に
+app.config["JSON_AS_ASCII"] = False
+
+@app.route("/")
+def index():
+    return "index page"
+
+@app.route('/users/<int:id>')
+def user(id):
+    stmt = 'SELECT * FROM users WHERE id = ?'
+    user = db.query(stmt, id, prepared=True)
+    print(user)
+    return jsonify({"user_id": user[0][0], "user_name": user[0][1]}), 200
